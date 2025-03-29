bc73c83d12c5d179b837a66815e642c8340343ad
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-03-29 20:11:24 +0200

    Add box2d directly as c library

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
-const Build = @import("std").Build;
+const std = @import("std");
 const box2d = @import("Box2D.zig/build.zig");
 
-pub fn build(b: *Build) !void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -12,20 +12,6 @@ pub fn build(b: *Build) !void {
         .optimize = optimize,
     });
     exe.linkLibC();
-
-    // const sdl_dep = b.dependency("sdl", .{
-    //     .optimize = optimize,
-    //     .target = target,
-    // });
-    // exe.linkLibrary(sdl_dep.artifact("SDL2"));
-    // const sdl_image_dep = b.dependency("sdl_image", .{
-    //     .optimize = optimize,
-    //     .target = target,
-    // });
-    // exe.linkLibrary(sdl_image_dep.artifact("SDL2_image"));
-
-    // exe.linkSystemLibrary("SDL2");
-    // exe.linkSystemLibrary("SDL2_image");
 
     exe.addLibraryPath(.{ .cwd_relative = "/usr/local/opt/sdl2_image/lib" });
     exe.addLibraryPath(.{ .cwd_relative = "/usr/local/opt/sdl2/lib" });
@@ -38,11 +24,43 @@ pub fn build(b: *Build) !void {
     exe.root_module.addImport("zsdl2", zsdl.module("zsdl2"));
     exe.root_module.addImport("zsdl2_image", zsdl.module("zsdl2_image"));
 
-    const box2dModule = try box2d.addModule(b, "Box2D.zig", .{
-        .optimize = optimize,
-        .target = target,
-    });
-    exe.root_module.addImport("box2d", box2dModule);
+    exe.addCSourceFiles(.{ .root = b.path("./box2d/src/"), .files = &[_][]const u8{
+        "aabb.c",
+        "arena_allocator.c",
+        "array.c",
+        "bitset.c",
+        "body.c",
+        "broad_phase.c",
+        "constraint_graph.c",
+        "contact.c",
+        "contact_solver.c",
+        "core.c",
+        "distance.c",
+        "distance_joint.c",
+        "dynamic_tree.c",
+        "geometry.c",
+        "hull.c",
+        "id_pool.c",
+        "island.c",
+        "joint.c",
+        "manifold.c",
+        "math_functions.c",
+        "motor_joint.c",
+        "mouse_joint.c",
+        "prismatic_joint.c",
+        "revolute_joint.c",
+        "sensor.c",
+        "shape.c",
+        "solver.c",
+        "solver_set.c",
+        "table.c",
+        "timer.c",
+        "types.c",
+        "weld_joint.c",
+        "wheel_joint.c",
+        "world.c",
+    } });
+    exe.addIncludePath(b.path("./box2d/include/"));
 
     b.installArtifact(exe);
 
