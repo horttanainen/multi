4f4e3ce70188a5a55d79753c7b6d4ce7bf45f76c
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-04-26 21:01:56 +0300

    Bound camera to level

--- src/camera.zig
+++ src/camera.zig
@@ -1,11 +1,15 @@
 const vec = @import("vector.zig");
 
+const box = @import("box.zig");
+const level = @import("level.zig");
 const config = @import("config.zig");
 const shared = @import("shared.zig");
 const player = @import("player.zig");
 const entity = @import("entity.zig");
 
-var camPos: vec.IVec2 = .{ .x = 0, .y = 0 };
+const conv = @import("conversion.zig");
+
+pub var camPos: vec.IVec2 = .{ .x = 0, .y = 0 };
 
 pub fn relativePosition(pos: vec.IVec2) vec.IVec2 {
     return vec.isubtract(pos, camPos);
@@ -20,4 +24,23 @@ pub fn followPlayer() void {
 fn move(pos: vec.IVec2) void {
     camPos.x = pos.x - config.window.width / 2;
     camPos.y = pos.y - config.window.height / 2;
+
+    if (level.maybeLevel) |l| {
+        const levelWidthHalf = @divFloor(conv.m2P(l.sprite.dimM.x), 2);
+        const levelHeightHalf = @divFloor(conv.m2P(l.sprite.dimM.y), 2);
+
+        //TODO: store levelPosition in level as pixels
+        const levelPos = conv.m2Pixel(box.getState(l.bodyId).pos);
+
+        if (camPos.x < levelPos.x - levelWidthHalf) {
+            camPos.x = levelPos.x - levelWidthHalf;
+        } else if (camPos.x > levelPos.x + levelWidthHalf - config.window.width) {
+            camPos.x = levelPos.x + levelWidthHalf - config.window.width;
+        }
+        if (camPos.y < levelPos.y - levelHeightHalf) {
+            camPos.y = levelPos.y - levelHeightHalf;
+        } else if (camPos.y > levelPos.y + levelHeightHalf - config.window.height) {
+            camPos.y = levelPos.y + levelHeightHalf - config.window.height;
+        }
+    }
 }
