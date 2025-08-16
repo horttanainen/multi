b947723486c928a18d404c6ac63921720e6fe48d
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-08-16 17:25:15 +0300

    Tick off todos

--- src/main.zig
+++ src/main.zig
@@ -30,8 +30,6 @@ const Entity = entity.Entity;
 const Sprite = entity.Sprite;
 
 //Single player game todos:
-//TODO: add parallax background
-//TODO: add idle animation
 //TODO: add walking animation
 //TODO: add sensor to projectile to explode on first collision
 //TODO: add visualisation for explosion
